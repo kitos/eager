63437869c1687c6cc0242f37a00f1996d2ad493c
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2016-04-16 18:21:34 +0300

    goals are now stored in mongodb

--- src/server/controllers/goals.controller.js
+++ src/server/controllers/goals.controller.js
@@ -1,13 +1,59 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+var GoalSchema = require('./../schemas/goal.schema');
+
+var Goal = mongoose.model('goals', GoalSchema);
 
 module.exports = () => {
     var router = express.Router();
 
     router.route('/goals')
-        .get((request, response) => response.json([
-            {id: 1, description: 'my first goal'},
-            {id: 2, description: 'my second goal'}
-        ]));
+        // get all goals
+        .get((request, response) => Goal.find().exec()
+            .then(goals => response.json(goals)))
+        // add new goal
+        .post((request, response) => {
+            (new Goal({
+                title: request.body.title,
+                description: request.body.description
+            }).save())
+                .then(goal => response.json(goal))
+                .catch(err => {
+                    response.status(500);
+                    response.send(err);
+                });
+        });
+
+    router.route('/goals/:id')
+        // get goal by id
+        .get((request, response) => Goal.findById(request.params.id)
+            .then(goal => response.json(goal))
+            .catch(err => {
+                response.status(500);
+                response.send(err);
+            })
+        )
+        // update goal
+        .put((request, response) => {
+            Goal.findById(request.params.id)
+                .then(goal => {
+                    goal.title = request.body.title;
+                    goal.description = request.body.description;
+                    return goal.save();
+                })
+                .then(updatedGoal => response.json(updatedGoal))
+                .catch(err => {
+                    response.status(500);
+                    response.send(err);
+                })
+        })
+        .delete((request, response) => Goal.findOneAndRemove(request.params.id).exec()
+            .then(goal => response.json(goal))
+            .catch(err => {
+                response.status(500);
+                response.send(err);
+            }));
 
     return router;
 };
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var goalsApi = require('./controllers/goals.controller');
 
 var app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use(goalsApi());
 
-app.listen(3000, () => console.log('App is listening on  http://localhost:3000'));
+mongoose.connect('mongodb://admin:123123@ds030829.mlab.com:30829/eager');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Successful db connection.');
+    app.listen(3000, () => console.log('App is listening on  http://localhost:3000'));
+});
