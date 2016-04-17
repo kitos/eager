a0ca6677e562d86be2892c04b69c3f8a7c559251
Pavel Smirnov <pavelsmirnovvv@gmail.com>
2016-04-17 08:18:53 +0300

    Log time dialog with date picker

--- src/app/components/dom/log-time-dialog.component.js
+++ src/app/components/dom/log-time-dialog.component.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Dialog from 'material-ui/lib/dialog';
 import FlatButton from 'material-ui/lib/flat-button';
 import TextField from 'material-ui/lib/text-field';
+import DatePicker from 'material-ui/lib/date-picker/date-picker';
 
 export default class LogTimeDialog extends React.Component {
 
@@ -17,22 +18,34 @@ export default class LogTimeDialog extends React.Component {
 
     render() {
         var actions = [
-            <FlatButton label="Save" primary={true} onTouchTap={() => this.props.onSubmit(this.state.goal, this.state.timelog)}/>,
+            <FlatButton label="Save" primary={true}
+                        onTouchTap={() => this.props.onSubmit(this.state.goal, this.state.timelog)}/>,
             <FlatButton label="Cancel" onTouchTap={this.props.onDialogClose}/>
         ];
 
         return (
             <Dialog open={this.props.open} onRequestClose={this.props.onDialogClose}
                     actions={actions}>
-                <TextField floatingLabelText="Hours" fullWidth={true} value={this.state.timelog.time} onChange={e => this.handleTimeChange(e.target.value)}/>
+                <TextField floatingLabelText="Hours" fullWidth={true} value={this.state.timelog.time}
+                           onChange={e => this.handleTimeChange(e.target.value)}/>
                 <br/>
+                <DatePicker value={this.state.timelog.date}
+                            disableYearSelection={true}
+                            defaultDate={new Date()}
+                            
+                            onChange={(oldval, newval) => this.handleDateChange(newval)}/>
             </Dialog>
         );
     }
 
-    handleTimeChange(newTitle) {
-        this.state.timelog.time = newTitle;
+    handleTimeChange(newTime) {
+        this.state.timelog.time = newTime;
         this.setState(this.state);
     }
-    
+
+    handleDateChange(newDate) {
+        this.state.timelog.date = newDate;
+        this.setState(this.state);
+    }
+
 }
