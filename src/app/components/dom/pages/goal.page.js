e440a4ab6eafe9f078c931e76595272cf2970936
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2016-04-17 06:50:22 +0300

    goal page shows goal info now

--- src/app/components/dom/pages/goal.page.js
+++ src/app/components/dom/pages/goal.page.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
-const GoalPage = () => (
-    <div>This is goal page</div>
-);
+import TextField from 'material-ui/lib/text-field';
 
-export default GoalPage;
+const GoalPage = ({goals, goalId}) => {
+    var goal = goals.find(g => g._id == goalId);
+    return goal ? (
+        <div>
+            <TextField floatingLabelText="Title" fullWidth={true} value={goal.title} disabled={true}/>
+            <br/>
+            <TextField floatingLabelText="Description" fullWidth={true} value={goal.description} disabled={true}/>
+        </div>
+
+    ) : <div>Loading...</div>;
+};
+
+export default connect(
+    state => state.goals,
+    (dispatch, ownProps) => ({goalId: ownProps.params.goalId})
+)(GoalPage);
